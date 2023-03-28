8716b5e79268b34a31fdd595075f2b91230e636c
amitha-priyangi <81142542+amitha-priyangi@users.noreply.github.com>
2023-03-28 17:02:28 +0530

    style add html code

--- js/main.js
+++ js/main.js
@@ -1,48 +1,43 @@
-const searchName=document.getElementById("searchBox");
-const suggestionList=document.getElementById('suggestions');
-const resultDiv=document.getElementById('result');
+const searchName = document.getElementById("searchBox");
+const resultDiv = document.getElementById('result');
 
-document.getElementById('search').addEventListener('click',search);
+document.getElementById('search').addEventListener('click', search);
 
 function search() {
     const username = searchName.value;
-    fetch(`https://api.github.com/users/${username}`).then(response => response.json()).then(data => {
-        const resultDiv = document.getElementById("result");
+    fetchUserDetails(username);
+}
 
+async function fetchUserDetails(username) {
+    try {
+        //   https://api.github.com/search/users?q=Q
+      const response = await fetch(`https://api.github.com/users/${username}?q=Q`);
+    
+      if (response.ok) {
+        const data = await response.json();
+        displayUserDetails(data)
+      } else {
+        const resultError = document.getElementById("result");
+        resultError.innerHTML=`<h2>User not found</h2>`
+        //  throw new Error('User not found');
+      }
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+  
+  
+  async function displayUserDetails(data) {
+    const resultDiv = document.getElementById("result");
         resultDiv.innerHTML = `
-          <h2>${data.login}</h2>
-          <img src="${data.avatar_url}" alt="Profile picture">
-          <ul>
+        <h3>Hi! ${data.login}</h3>
+        <img src="${data.avatar_url}" alt="Profile picture">
+        <ul>
             <li>Username: ${data.login}</li>
             <li>Followers: ${data.followers}</li>
             <li>Following: ${data.following}</li>
             <li>Public Repositories: ${data.public_repos}</li>
-          </ul>
+        </ul>
         `;
-      })
-      .catch(error => {console.log(error);
-        });
   }
-
-
-
-// fetch user details from GitHub API
-// const fetchUserDetails = async (username)=>{
-//     try{
-//         const response= await fetch(`https://api.github.com/users/${username}`);
-//         console.log(response);
-//         if(response.ok){
-//             const data=await response.json(); //returns a promise resolved to a JSON object
-//             return data;
-//         }else{
-//             throw new Error('User not found');
-//         }
-//     }catch(error){
-//         console.error(error);
-//         return null;
-//     }
-
-// }
-
-// display user details and repositories in resultDiv
-
