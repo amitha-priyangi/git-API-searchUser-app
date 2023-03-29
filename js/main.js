072aa22544b1af67eca4631b98a66a054732d799
amitha-priyangi <81142542+amitha-priyangi@users.noreply.github.com>
2023-03-29 17:09:45 +0530

    displa repositeris

--- js/main.js
+++ js/main.js
@@ -1,43 +1,147 @@
 const searchName = document.getElementById("searchBox");
+const suggestionList = document.getElementById('suggestions');
 const resultDiv = document.getElementById('result');
 
 document.getElementById('search').addEventListener('click', search);
 
 function search() {
-    const username = searchName.value;
-    fetchUserDetails(username);
+  const username = searchName.value;
+  fetchUserDetails(username);
+
 }
 
+// add event listener to searchInput to trigger searchUsers on input
+// async function searchUsers() {
+//   const searchText = searchName.value.trim();
+//   if (searchText.length < 1) {
+//     suggestionList.innerHTML = '';
+//     return;
+//   }
+//   try {
+//     const resSugges = await fetch(`https://api.github.com/search/users?q=${searchText}`);
+//     if (resSugges.ok) {
+//       const suggesData = await resSugges.json();
+//       displaySuggestions(suggesData.items);
+//     } else {
+//       throw new Error('Failed to fetch suggestions');
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     suggestionList.innerHTML = '';
+//   }
+// }
+// searchName.addEventListener('input', searchUsers);
+
+ 
+
+
 async function fetchUserDetails(username) {
-    try {
-        //   https://api.github.com/search/users?q=Q
-      const response = await fetch(`https://api.github.com/users/${username}?q=Q`);
-    
-      if (response.ok) {
-        const data = await response.json();
-        displayUserDetails(data)
-      } else {
-        const resultError = document.getElementById("result");
-        resultError.innerHTML=`<h2>User not found</h2>`
-        //  throw new Error('User not found');
-      }
-    } catch (error) {
-      console.error(error);
-      return null;
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}?q=Q`);
+    if (response.ok) {
+      const data = await response.json();
+      displayUserDetails(data)
+    } else {
+      const resultError = document.getElementById("result");
+      resultError.innerHTML = `<h2>User not found</h2>`
     }
+  } catch (error) {
+    console.error(error);
+    return null;
   }
-  
-  
-  async function displayUserDetails(data) {
-    const resultDiv = document.getElementById("result");
-        resultDiv.innerHTML = `
-        <h3>Hi! ${data.login}</h3>
-        <img src="${data.avatar_url}" alt="Profile picture">
-        <ul>
-            <li>Username: ${data.login}</li>
-            <li>Followers: ${data.followers}</li>
-            <li>Following: ${data.following}</li>
-            <li>Public Repositories: ${data.public_repos}</li>
-        </ul>
+}
+
+
+async function displayUserDetails(data) {
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = `
+        <div class="clo-md-12 search-col">
+                <div class="details">
+                    <img src="${data.avatar_url}" alt="Profile picture">
+                    <h3>Hi! ${data.login}</h3>
+                    <a href="${data.html_url}" target="_blank">View Profile on GitHub</a>
+                </div>
+            </div>
+            <div class="col-md-4 de-1">
+                <h6>${data.followers}</h6>
+                <h5>Followers</h5>
+            </div>
+            <div class="col-md-4 de-1">
+                <h6>${data.following}</h6>
+                <h5>Following</h5>
+            </div>
+            <div class="col-md-4 de-1">
+                <h6>${data.public_repos}</h6>
+                <h5>Public Repositories</h5>
+            </div>
         `;
+        
+        displayRepos(data)
+        
+}
+
+async function displayRepos(data) {
+  const repoCol = document.getElementById("repo");
+  try {
+    const responseRepo = await fetch(`https://api.github.com/users/${data.login}/repos`);
+    if (responseRepo.ok) {
+      const repos = await responseRepo.json();
+      repoCol.innerHTML = `
+          <h2>Repositories</h2>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Url</th>
+                      <th>Language</th>
+                      <th>Stars</th>
+                      <th>Forks</th>
+                    </tr>
+                  </thead>
+                  <tbody id="repos"></tbody>
+                </table>
+          `;
+
+      const reposTable = document.getElementById("repos");
+      const rows = repos.map(data => {
+        return `
+          <tr>
+            <td><a href="${data.name}" target="_blank">${data.name}</a></td>
+            <td>${data.url || 'No description provided.'}</td>
+            <td>${data.language || 'N/A'}</td>
+            <td>${data.stargazers_count}</td>
+            <td>${data.forks_count}</td>
+          </tr>
+        `;
+      });
+      
+      reposTable.innerHTML = rows.join('');
+    } else {
+      throw new Error('Failed to fetch repositories');
+    }
+  } catch (error) {
+    console.error(error);
   }
+}
+
+// function displaySuggestions(suggesData) {
+//   suggestionList.innerHTML = '';
+//   suggesData.map(user => {
+//     const li = document.createElement('li');
+//     li.innerHTML = `
+//       <img src="${user.avatar_url}" alt="${user.login}">
+//       <span>${user.login}</span>
+//     `;
+//     li.addEventListener('click', async () => {
+//       const userDetails = await fetchUserDetails(user.login);
+//       if (userDetails) {
+//         displayUserDetails(userDetails);
+//       }
+//     });
+//     suggestionList.appendChild(li);
+//   });
+// }
+
+
+
+
